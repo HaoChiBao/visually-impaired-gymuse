d1bb3add4c2720477c2d24bf8a27fb60d837d8d5
James Yang <jamesyang663@gmail.com>
2024-02-23 17:04:38 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+const main = async () => {
+  const testElement = document.querySelector('p')
+
+  
+}
+
+window.addEventListener('load', async () => {
+  await main()
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>
+        Text Goes Here...
+      </p>
+    </>
   );
 }
 
