52138fc8bcc11425f28f613552eeac50846e4fee
James Yang <jamesyang663@gmail.com>
2024-03-15 14:13:11 -0400

    register details good

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -21,15 +21,17 @@ const introText = [
     // 'I will ask you a few personal questions to get to know you better. I will repeat your answers back to you, all you have to do to confirm is say yes or no',
     // 'You will also be able to edit your responses directly on the screen',
     // 'If I go too fast you can say "REPEAT" to hear the information again or say "BACK" to go to the previous page',
-    'Ready to begin?'
-
+    'Ready to begin?',
 ]
 let introCounter = 0
 
 const questionsTest = [
     'What is your name? Say "MY NAME IS", and then followed by your name',
+    'Is your name',
     'What is your approximate weight in kilograms? Say "MY WEIGHT IS", and then followed by your weight',
+    'Is your weight',
     'What is your age? Say "MY AGE IS", and then followed by your age',
+    'Is your age',
 ]
 
 let questionCounter = 0
@@ -57,6 +59,7 @@ let timeout = null // timeout to wait for user to finish speaking
 const Register = () => {
     const [speech, setSpeech] = useState(introText[0])
     const [transcript, setTranscript] = useState('what you say appears here...')
+    const [userInput, setUserInput] = useState('')
     let speechOn = false
 
     let recognition = new window.webkitSpeechRecognition;
@@ -81,14 +84,27 @@ const Register = () => {
             // console.log(error)
         }
     }
+    
+    const openUserInput = (value = '') => {
+        const input = document.querySelector('input')
+        input.style.opacity = 1
+        input.style.pointerEvents = 'all'
+        input.value = value
+    }
+
+    const closeUserInput = () => {
+        const input = document.querySelector('input')
+        input.style.opacity = 0
+        input.style.pointerEvents = 'none'
+    }
 
     recognition.onresult = (event) => {
         const last = event.results.length - 1
         let speech = event.results[last][0].transcript
         speech = speech.toLowerCase()
+        setTranscript(speech)
         clearTimeout(timeout)
         timeout = setTimeout(() => {
-            setTranscript(speech)
             triggerWords.forEach(async (word) => {
                 if (speech.includes(word)) {
                     turnRecognitionOff()
@@ -106,14 +122,17 @@ const Register = () => {
                         case 'my name is':
                             const name = speech.split('my name is')[1].trim()
                             userDetails.name = name
+                            nextPage()
                             break
                         case 'my weight is':
                             const weight = getNumberFromString(speech)[0]
                             userDetails.weight = weight
+                            nextPage()
                             break
                         case 'my age is':
                             const age = getNumberFromString(speech)[0]
                             userDetails.age = age
+                            nextPage()
                             break
 
                         default:
@@ -137,7 +156,13 @@ const Register = () => {
             introCounter++
             setSpeech(introText[introCounter])
         } else if (questionCounter < questionsTest.length) {
-            setSpeech(questionsTest[questionCounter])
+            if(questionCounter % 2 === 0) {
+                setSpeech(questionsTest[questionCounter])
+                closeUserInput()
+            } else {
+                setSpeech(questionsTest[questionCounter] + ' ' + userDetails[questionsTest[questionCounter].split(' ')[2]])
+                openUserInput(userDetails[questionsTest[questionCounter].split(' ')[2]])
+            }
             questionCounter++
         }
     }
@@ -204,6 +229,7 @@ const Register = () => {
             </div>
             <div className="bottom">
                 <p>{speech}</p>
+                <input type="text" placeholder='details...'/>
             </div>
             <SpeechFooter speech={transcript}/>
         </div>
