af58a64b1b02d595f021ea2517127def5e10d5dc
James Yang <jamesyang663@gmail.com>
2024-03-26 15:35:27 -0400

    generateSpeech change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -156,7 +156,7 @@ const Home = () => {
       turnRecognitionOn()
     } else {
       console.log('error')
-      turnRecognitionOff()
+      // turnRecognitionOff()
     }
     
     isSpeaking = false
