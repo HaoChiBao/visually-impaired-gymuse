2b38241db6257a27b8ee2d4bd5874f104a9275fd
James Yang <jamesyang663@gmail.com>
2024-04-02 02:52:53 -0400

    test

--- src/functions/generateText.js
+++ src/functions/generateText.js
@@ -5,16 +5,11 @@ const apiUrl = `https://api.openai.com/v1/audio/transcriptions`;
 const model = 'whisper-1';
 
 const generateText = async (audioBlob) => {
-    console.log(audioBlob)
-    // save audio blob as a local file
-    // const audioFile = new File([audioBlob], 'audio.webm', {type: 'audio/webm'});
-    const blob = new Blob([audioBlob], {type: 'audio/webm'});
-    // console.log(blob)
 
     const formData = new FormData();
     formData.append('model', model);
     // formData.append('file', audioBlob);
-    formData.append('file', blob, 'audio.webm');
+    formData.append('file', audioBlob, 'audio.webm');
     // define language type ENGLISH
     formData.append('language', 'en'); // Set language to English
 
